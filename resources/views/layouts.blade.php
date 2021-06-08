e4d71304dde8eda0318aaea0388bc507fd514474
EC2 Default User <ec2-user@ip-172-31-32-12.us-east-2.compute.internal>
2021-06-08 14:07:58 +0000

    最初のコミット

---
+++ resources/views/layouts.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link 
+        rel="stylesheet" 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" 
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+        crossorigin="anonymous"
+    >
+    <title>人生逆転サロン</title>
+</head>
+<body>
+    <header class="navbar nabibar-dark bg-danger">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('') }}">
+            Gyakuten
+        </a>
+    </div>
+    </header>
+    
+    @yield('content')
+    
+</body>
+</html>
