93e11194b9f1ba9e86655c7ae700aeb2fa5ed2f6
EC2 Default User <ec2-user@ip-172-31-32-12.us-east-2.compute.internal>
2021-06-10 08:04:42 +0000

    課題５までの実装

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -12,5 +12,22 @@ class PostsController extends Controller
         $posts = Post::orderBy('created_at','desc')->get();
         return view('posts.index', ['posts' => $posts]);
     }
+    
+    public function create()
+    {
+        return view('posts.create');
+    }
+    
+    public function store(Request $requeset)
+    {
+        $params = $requeset->validate([
+            'title' => 'required|max:20',
+            'body' => 'required|max:140',
+            ]);
+            
+        Post::create($params);
+        
+        return redirect()->route('top');
+    }
 }
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,6 +4,11 @@
 
 
 <div class="container mt-4">
+    <div class="mb-4">
+        <a href ="{{route('posts.create')}}" class="btn btn-primary">
+            投稿を新規作成する
+        </a>
+    </div>
     @foreach($posts as $post)
     <div class="card mt-4">
         <div class="card-header mb-2">
